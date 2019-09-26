603785b0089f5a043eddc97b15d1423d0f96db4d
Axel <axel@nam.se>
2019-09-26 19:36:05 +0200

    up

---
+++ Test/Assets/CameraControlle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControlle : MonoBehaviour
+{
+    public Transform target;
+
+    public Vector3 offset;
+
+    public bool useOffsetValues;
+
+    public float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get the X position of the mouse & rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+
+        //Move the camera based on the current rotation of the target & the original offset
+        float desiredYAngle = target.eulerAngles.y;
+
+        Quaternion rotation = Quaternion.Euler(0, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position = target.position - offset;
+    
+        transform.LookAt(target);
+
+    }
+}
